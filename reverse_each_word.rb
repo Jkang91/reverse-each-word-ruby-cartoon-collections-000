18e0a92a3618e4b3eeadf67d80c92d7d0dd676cf
June Young Kang <jkang34@gmail.com>
2020-10-18 19:46:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,19 @@
 greeting = "Hello there, how are you today?"
 
 
-
 def reverse_each_word(greeting)
   greeting.split.collect{|word| word.reverse}.join(" ")
 end
+
+# def reverse_each_word(greeting)
+#   new_greeting = []
+#   greeting.split.each do |word|
+#     word = word.reverse 
+#     new_greeting << word
+#   end
+#   return new_greeting.join(" ")
+# end
+
+
+  
+
